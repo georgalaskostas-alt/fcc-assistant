04ab9969f4e47ce0de2c941f1db2f32b7849bc35
georgalaskostas-alt <georgalas.kostas@gmail.com>
2026-08-23 17:14:25 +0300

    Load voice command styles

--- desktop/src/main.tsx
+++ desktop/src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App";
 import "./styles.css";
 import "./WorkspaceResponsive.css";
 import "./EnterpriseTheme.css";
+import "./Speech.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
